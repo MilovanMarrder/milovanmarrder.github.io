ace38afc3dbbeda20fcf3d19e28ee878af782498
MilovanMarrder <milovanmarrder@outlook.com>
2021-07-31 16:06:12 -0600

    Formulario listo

--- Firma3.php
+++ Firma3.php
@@ -13,77 +13,89 @@ if (isset($_POST['submit'])) {
 
 ?>
 <div class="firma3caja">
-	<table>
-		<tbody>
-			<tr>
-				<!-- En el siguiente td está el logo -->
-				<td><a href="
-				<?php
-				echo "$web_empresa";
+<table
+  style="
+    line-height: 1;
+    font-family: BlinkMacSystemFont, -apple-system, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, Helvetica, Arial,
+      sans-serif;
+  "
+>
+  <tbody>
+    <tr>
+      <td style="padding: 0px 15px 0px 0px; text-align: center; width: 114px">
+        <img
+          src="<?php echo "$logo";?>"
+          alt="Logo de la empresa"
+          width="150"
+          height="92"
+          border="0"
+        />
+      </td>
+      <td
+        style="
+          width: 456px;
+          border-left: 1px solid;
+          border-color: #cccccc;
+          padding: 0px 0px 0px 15px;
+        "
+      >
+        <p style="font-size: 18px; font-weight: bold; color: #0b486b">
+		<?php echo "$titulo $nombre"; ?>
+        </p>
+        <p
+          style="
+            margin-top: -10px;
+            font-size: 16px;
+            font-weight: normal;
+            color: #4a4a4a;
+          "
+        ><?php	echo $area;?>
+        </p>
+        <p style="font-size: 14px; font-weight: normal; color: #4a4a4a">
+		<?php if ($mail != null) { echo $mail;} ?> | 
+		<?php	if ($cel != null) {	echo "Cel: $cel";}?>
+        </p>
+        <table style="border-collapse: collapse; width: 27.2702%" border="0">
+          <tbody>
+            <tr>
+              <td style="width: 7.73521%"></td>
+              <td style="width: 10.606%">
+                <a
+                  style="padding: 10px"
+                  href="https://www.linkedin.com/company/segara-environmental-consulting/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  ><img
+                    src="https://nextcloud.segara.io/index.php/s/BAAPzo4NLdkaaWB/preview"
+                    alt="Icono de LinkedIn"
+                    width="25"
+                    height="25"
+                /></a>
+              </td>
+              <td style="width: 8.93143%">
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </td>
+    </tr>
+    <tr>
+      <td
+        style="padding: 20px 20px 0px 0px; width: 571px"
+        colspan="2"
+        valign="middle"
+      >
+      </td>
+    </tr>
+    <tr>
+      <td
+        style="padding: 20px 20px 0px 0px; vertical-align: top; width: 571px"
+        colspan="2"
+        valign="middle"
+      ></td>
+    </tr>
+  </tbody>
+</table>
 
-				?>
-				"><img src="
-				<?php
-				echo "$logo"; ?>
-				" width="150" height="92" /></a></td>
-				<!-- Aquí termina el td del logo -->
-				<!-- Estas td (Columna) conforman el separador vertical de color -->
-				<td style="width: 13px;"></td>
-				<td style="width: 2px; background-color: var(--naranja);"></td>
-				<td style="width: 13px;"></td>
-				<!-- Hasta aquí el separador vertical (Verificar si se puede agregar sombra) -->
-				<!-- Aqui inicia la tablita de el texto -->
-				<td>
-					<table class="firma1texto">
-						<tbody>
-							<tr>
-								<td style=" font-size:15px; color:#4c4b4c; font-weight:bold;">
-									<?php echo "$titulo $nombre"; ?>
-								</td>
-							</tr>
-							<tr>
-
-								<td class="celdaarea">
-									<?php
-									echo $area;
-									?>
-								</td>
-							</tr>
-							<tr>
-								<td style="height: 14px;">
-								<?php
-								if ($cel != null) {
-									echo "Cel: $cel";
-								}
-								?>
-							</td>
-							</tr>
-
-							<tr>
-
-								<td>
-									<?php
-									if ($tel != null) {
-										echo "Tel: $tel";
-									}
-									if ($ext != null) {
-										echo " Ext: $ext";
-									} ?>
-								</td>
-							</tr>
-							<tr>
-								<td><a href="mailto:$mail">
-										<?php
-										if ($mail != null) {
-											echo $mail;
-										} ?>
-									</a>
-								</td>
-							</tr>
-						</tbody>
-					</table>
-				</td>
-			</tr>
-		</tbody>
-	</table>
 </div>
--- form.php
+++ form.php
@@ -1,50 +1,65 @@
 <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" enctype="multipart/form-data" class="formulario">
     <h2 style="color:rgb(35, 35, 35);">GENERANDO TU FIRMA DE CORREO</h2>
     <div>
-        <label for="Nombre">Nombre</label>
-        <Input type="text" name="nombre" id="Nombre1" placeholder="Juan Francisco"></Input>
-        <!-- Los agrego como botones pero lo podemos dejar con la lista que sugeriste -->
-        <br>
-        <select name="titulo" id="titulo">
-            <option value="">Ninguno</option>
-            <option value="Lic.">Lic.</option>
-            <option value="Licda.">Licda.</option>
-            <option value="Ing.">Ing.</option>
-            <option value="Dr.">Dr.</option>
-            <option value="Dra.">Dra.</option>
-            <option value="Oficial">Oficial</option>
-        </select>
-        <br>
-        <label for="area">Departamento</label>
-        <Input type="text" id="area" name="area" placeholder="Departamento o área"></Input>
-        <label for="cel">Teléfono movil</label>
-        <Input type="text" id="cel" name="cel" placeholder=" +504 32772517"></Input>
-        <label for="tel">Teléfono de la empresa</label>
-        <Input type="text" id="tel" name="tel" placeholder="22360900">
-        <label for="ext">Extensión</label>
-        <Input type="text" id="ext" name="ext" placeholder="13110"></Inpuut>
-        <label for="mail">Correo electrónico</label>
-        <Input type="text" id="mail" name="mail" placeholder="correo@hospitalmaria.org"></Input>
-        <label for="ubicacion">Ubicación</label>
-        <Input type="text" id="ubicacion" name="ubicacion" placeholder="Tegucigalpa, Honduras" cols="10" row="5"></Input>
-        <br>
-        <br>
-        
+        <div class="row">
+            <div class="col">
+                <select name="titulo" id="titulo">
+                    <option value="">Ninguno</option>
+                    <option value="Lic.">Lic.</option>
+                    <option value="Licda.">Licda.</option>
+                    <option value="Ing.">Ing.</option>
+                    <option value="Dr.">Dr.</option>
+                    <option value="Dra.">Dra.</option>
+                    <option value="Oficial">Oficial</option>
+                </select>
+                <Input type="text" name="nombre" id="Nombre1" placeholder="Ingresa el nombre a mostrar"></Input>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <Input type="text" id="area" name="area" placeholder="Departamento o área"></Input>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <Input type="text" id="cel" name="cel" placeholder="Teléfono movil"></Input>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <Input type="text" id="tel" name="tel" placeholder="Teléfono de la empresa">
+                <Input class="inputext" type="text" id="ext" name="ext" placeholder="Extesión"></Inpuut>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <Input type="text" id="mail" name="mail" placeholder="correo@outlook.com"></Input>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <Input type="text" id="ubicacion" name="ubicacion" placeholder="Tegucigalpa, Honduras" cols="10" row="5"></Input>
+            </div>
+        </div><br>
+
         <!--  
         <label for="foto">URL de fotografía (Opcional)</label>
         <Input type="text" id="foto" name="foto" placeholder="Pega aquí el URL de tu foto"></Inpuut>
         -->
-        <label for="logo">URL de logotipo de la empresa</label>
-        <Input type="text" id="logo" name="logo" placeholder="Pega aquí el URL del logo de tu empresa"></Inpuut>
-        <label for="web">Página web de la empresa</label>
-        <Input type="text" id="web" name="web" placeholder="Página web de la empresa"></Inpuut>
-        <br>
-        <br>
+        <div class="row">
+            <div class="col">
+                <Input type="text" id="logo" name="logo" placeholder="Pega aquí el URL del logo de tu empresa"></Inpuut>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col">
+                <Input type="text" id="web" name="web" placeholder="Enlace al hacer clic en el logo"></Inpuut>
+            </div>
+        </div>
     </div>
+        <br>
     <div class="botones">
-        <input type="submit" class="botones" name="submit" value="Guardar">
-        <input type="reset" class="botones" value="Limpiar campos">
-    </div>
-    <br>
-
+        <input type="submit" class="BotonDeFormulario" name="submit" value="Guardar">
+        <input type="reset" class="BotonDeFormulario" value="Limpiar campos">
+    </div>   
 </form>
