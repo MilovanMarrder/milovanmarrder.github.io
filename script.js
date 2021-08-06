5b340663e80131d7a8bfdb35d3bfe065a2eab074
MilovanMarrder <milovanmarrder@outlook.com>
2021-08-05 23:32:37 -0600

    Avance 02 con JS

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function V_nombre(texto) {
   document.querySelector("#Vista_nombre").innerHTML = texto;
 }
-function VisualiarTitulo(texto) {
-  document.querySelector("#Visualizartitulo").innerHTML = texto;
-}
 function V_Puesto(texto) {
   document.querySelector("#Vista_puesto").innerHTML = texto;
 }
 function V_cel(texto) {
   document.querySelector("#Vista_cel").innerHTML = texto;
 }
-function V_tel(texto) {
-  document.querySelector("#Vista_tel").innerHTML = texto;
+function V_Ext(texto) {
+  document.querySelector("#Vista_ext").innerHTML = texto;
 }
-function V_empresa(texto) {
-  document.querySelector("#Vista_empresa").innerHTML = texto;
+function V_dept(texto) {
+  document.querySelector("#Vista_departamento").innerHTML = texto;
 }
 function V_mail(texto) {
   document.querySelector("#Vista_mail").innerHTML = texto;
