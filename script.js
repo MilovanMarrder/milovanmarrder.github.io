1fad9571fe9ca566985f522cee82b9541b76815d
MilovanMarrder <milovanmarrder@outlook.com>
2021-08-07 16:42:48 -0600

    Finalizado estilos

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function V_cel(texto) {
     x.style.display = "none";
   }
 }
+
 function V_Ext(texto) {
   if (texto != "") {
     document.querySelector("#Vista_ext").innerHTML = "2236-0900 ext: " + texto;
@@ -34,16 +35,13 @@ function V_dept(texto) {
 function V_mail(texto) {
   document.querySelector("#Vista_mail").innerHTML = texto;
 }
-
-// input cel
-// display vista_cel
-// mostrar si tiene algo escrito o en este caso un checked
-/* function MostrarCel(texto) {
-
-
-  if (x != null) {
-    y.style.display = 'block';
-  } else {
-    y.style.display = 'none';
-  }
-} */
+function ejecutar(idelemento){
+  var aux = document.createElement("div");
+  aux.setAttribute("contentEditable", true);
+  aux.innerHTML = document.getElementById(idelemento).innerHTML;
+  aux.setAttribute("onfocus", "document.execCommand('selectAll',false,null)"); 
+  document.body.appendChild(aux);
+  aux.focus();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+}
